4f371d168f2cba81692b668ffc4aaf3509ca9eb1
P-dwivedi <prakhardwivedi31@gmail.com>
2024-09-27 00:02:45 +0530

    Added Clearing Forms feature

--- react-router-budget-app/src/pages/Dashboard.jsx
+++ react-router-budget-app/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@
 import { useLoaderData } from "react-router-dom";
 
 // helper functions
-import { createBudget, fetchData } from "../helpers"
+import { createBudget, fetchData, wait } from "../helpers"
 
 // components 
 import Intro from "../components/Intro";
@@ -21,6 +21,8 @@ export function dashboardLoader(){
 
 // action
 export async function dashboardAction({request}) {
+    await wait();
+
     const data = await request.formData();
     const {_action, ...values} = Object.fromEntries(data)
     // new user submission
