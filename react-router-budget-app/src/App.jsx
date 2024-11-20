ef40d8cdcd7bf9a3e18724452d393660ba3de219
P-dwivedi <prakhardwivedi31@gmail.com>
2024-11-21 04:14:57 +0530

    Add Expenses Page

--- react-router-budget-app/src/App.jsx
+++ react-router-budget-app/src/App.jsx
@@ -10,12 +10,14 @@ import 'react-toastify/dist/ReactToastify.css';
 // Routes
 import Dashboard, { dashboardAction, dashboardLoader } from "./pages/Dashboard";
 import Error from "./pages/Error";
+import ExpensesPage, { expensesLoader } from "./pages/ExpensesPage";
 
 // Layouts
 import Main, { mainLoader } from "./layouts/Main";
 
 // Actions
 import { logoutAction } from "./actions/logout";
+
 
 const router = createBrowserRouter([
   {
@@ -30,6 +32,11 @@ const router = createBrowserRouter([
         loader: dashboardLoader,
         action: dashboardAction,
         errorElement: <Error />
+      },
+      {
+        path: "expenses",
+        element: <ExpensesPage />,
+        loader: expensesLoader,
       },
       {
         path: "logout",
